e70d56c0e294d7f08521bf3fd998ab62e018ca5d
Andrew Lesh <adlesh8@gmail.com>
2019-02-06 21:34:30 -0500

    Added a  window to open after the button is pressed that leads to a test feedback form

--- collapse.js
+++ collapse.js
@@ -2,12 +2,13 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", () => {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
-    } else {
+    } 
+    else {
       content.style.display = "block";
     }
   });
