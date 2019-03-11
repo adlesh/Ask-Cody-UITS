0750bdd686d043938a5a27cbdd6eef091d9e8471
Andrew Lesh <adlesh8@gmail.com>
2019-03-11 10:15:11 -0400

    Tweaking vue-beautiful-chat

---
+++ ask-cody-vue/dev/src/colors.js
@@ -0,0 +1,50 @@
+export default {
+    red: {
+      header: {
+        bg: '#D32F2F',
+        text: '#fff'
+      },
+      launcher: {
+        bg: '#D32F2F'
+      },
+      messageList: {
+        bg: '#fff'
+      },
+      sentMessage: {
+        bg: '#F44336',
+        text: '#fff'
+      },
+      receivedMessage: {
+        bg: '#eaeaea',
+        text: '#222222'
+      },
+      userInput: {
+        bg: '#fff',
+        text: '#212121'
+      }
+    },    blue: {
+      header: {
+        bg: '#003359',
+        text: '#ffffff'
+      },
+      launcher: {
+        bg: '#003359'
+      },
+      messageList: {
+        bg: '#ffffff'
+      },
+      sentMessage: {
+        bg: '#003359',
+        text: '#ffffff'
+      },
+      receivedMessage: {
+        bg: '#fff',
+        text: '#003359'
+      },
+      userInput: {
+        bg: '#f4f7f9',
+        text: '#565867'
+      }
+    
+    }
+  }
